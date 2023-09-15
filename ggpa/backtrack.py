936d704525f64dd50864db8b512e394893cd691a
unknown <bbateni@ucsc.edu>
2023-09-15 01:34:34 -0700

    Add special case in backtrack when the game has ended, apply minor refactoring

--- ggpa/backtrack.py
+++ ggpa/backtrack.py
@@ -21,24 +21,25 @@ class BacktrackBot(GGPA):
     
     def _get_best_choose_card(self, game_state: GameState, battle_state: BattleState, depth_remaining: int) -> tuple[int|None, PlayCard|EndAgentTurn|None]:
         if depth_remaining == 0:
-            x = self._evaluate_state(game_state, battle_state)
-            return x, None
+            return self._evaluate_state(game_state, battle_state), None
         options = self.get_choose_card_options(game_state, battle_state)
         best_value, best_action = None, None
         for option in options:
             battle_state_copy: BattleState = deepcopy(battle_state)
             battle_state_copy.verbose = Verbose.NO_LOG
-            battle_state_copy.tick_player(option)
-            estimate = self._get_best_choose_card(battle_state_copy.game_state, battle_state_copy, depth_remaining-1)
-            if best_value is None or (estimate[0] is not None and best_value < estimate[0]):
-                best_value = estimate[0]
+            if not battle_state_copy.tick_player(option):
+                estimate = battle_state_copy.get_end_result() * 1000
+            else:
+                estimate, _ = self._get_best_choose_card(battle_state_copy.game_state, battle_state_copy, depth_remaining-1)
+            if best_value is None or (estimate is not None and best_value < estimate):
+                best_value = estimate
                 best_action = option
         return best_value, best_action
 
     def choose_card(self, game_state: GameState, battle_state: BattleState) -> EndAgentTurn|PlayCard:
         _, action = self._get_best_choose_card(game_state, battle_state, self.depth)
         if action is None:
-            raise Exception("Depth is 0 or no action available")
+            raise Exception("Depth is 0 or no action is available")
         return action
     
     def choose_agent_target(self, battle_state: BattleState, list_name: str, agent_list: list[Agent]) -> Agent:
