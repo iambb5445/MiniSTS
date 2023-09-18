e79ba349df61e414365d71d01c1da3342e2d8de2
unknown <bbateni@ucsc.edu>
2023-09-18 12:18:04 -0700

    Add parseargs for evaluate_bot and plotting, add evalute_deck

--- evaluate_bot.py
+++ evaluate_bot.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import pandas as pd
 import time
+import argparse
 from game import GameState
 from battle import BattleState
 from config import Character, Verbose
@@ -10,10 +11,23 @@ from ggpa.ggpa import GGPA
 from ggpa.random_bot import RandomBot
 from ggpa.backtrack import BacktrackBot
 
+def name_to_bot(name: str) -> GGPA:
+    if name == 'r':
+        return RandomBot()
+    if len(name) > 2 and name[0:2] == 'bt':
+        depth = int(name[2:])
+        return BacktrackBot(depth)
+    raise Exception("Bot name not recognized")
+
 def main():
-    test_count = 100
+    parser = argparse.ArgumentParser()
+    parser.add_argument('test_count', type=int)
+    parser.add_argument('bots', nargs='+')
+    args = parser.parse_args()
+
+    test_count = args.test_count
     results_dataset: list[tuple[str, GameState]] = []
-    bots: list[GGPA] = [RandomBot(), BacktrackBot(3)]
+    bots: list[GGPA] = [name_to_bot(name) for name in args.bots]
     for bot in bots:
         for _ in tqdm(range(test_count)):
             game_state = GameState(Character.IRON_CLAD, bot, 0)
@@ -25,7 +39,8 @@ def main():
         [[name, game_state.player.health, game_state.get_end_results() != -1] for name, game_state in results_dataset],
         columns=["BotName", "PlayerHealth", "Win"]
     )
-    df.to_csv(f"evaluation_results\\evaluation_on_{test_count}_tests_{int(time.time())}.csv", index=False)
+    bot_names = '_'.join([bot.name for bot in bots])
+    df.to_csv(f"evaluation_results\\evaluation_on_{test_count}_tests_{bot_names}_{int(time.time())}.csv", index=False)
 
 if __name__ == '__main__':
     main()
--- plot_evaluation.py
+++ plot_evaluation.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import seaborn as sns
+import argparse
 from matplotlib import pyplot as plt
 
-file_name: str = "evaluation_results\\evaluation_on_100_tests_1694812888.csv"
-
 def main():
-    df: pd.DataFrame = pd.read_csv(file_name)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('by_column')
+    args = parser.parse_args()
+    df: pd.DataFrame = pd.read_csv(args.filename)
 
+    by_column = args.by_column
+    test_count = len(df) / len(set(df[by_column]))
     plt.figure(figsize=(8, 6))
-    sns.histplot(data=df, x="PlayerHealth", hue="BotName", bins=15, element="step", common_norm=False, kde=True)
+    sns.histplot(data=df, x="PlayerHealth", hue=by_column, bins=15, element="step", common_norm=False, kde=True)
 
-    plt.title("Distribution of Player Health by Bot Name")
+    plt.title(f"Distribution of Player Health by {by_column} - {test_count} Samples")
     plt.xlabel("Player Health")
     plt.ylabel("Count")
     plt.show()
