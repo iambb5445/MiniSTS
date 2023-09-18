982fb89916ac887c1a97a807fc53c9beea4b14e5
unknown <bbateni@ucsc.edu>
2023-09-18 12:36:45 -0700

    Add insights such as mean and std of groups when plotting evaluation results

--- plot_evaluation.py
+++ plot_evaluation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import argparse
+import numpy as np
 from matplotlib import pyplot as plt
 
 def main():
@@ -10,13 +11,16 @@ def main():
     args = parser.parse_args()
     df: pd.DataFrame = pd.read_csv(args.filename)
 
-    by_column = args.by_column
-    test_count = len(df) / len(set(df[by_column]))
+    by_col = args.by_column
+    target_col = "PlayerHealth"
+    test_count = len(df) / len(set(df[by_col]))
+    print(df.filter(items=[target_col, by_col]).groupby(by_col).mean())
+    print(df.filter(items=[target_col, by_col]).groupby(by_col).apply(lambda g: np.mean(g[target_col] * g[target_col])))
+    print(df.filter(items=[target_col, by_col]).groupby(by_col).agg(np.std, ddof=1))
     plt.figure(figsize=(8, 6))
-    sns.histplot(data=df, x="PlayerHealth", hue=by_column, bins=15, element="step", common_norm=False, kde=True)
-
-    plt.title(f"Distribution of Player Health by {by_column} - {test_count} Samples")
-    plt.xlabel("Player Health")
+    sns.histplot(data=df, x=target_col, hue=by_col, bins=15, element="step", common_norm=False, kde=True)
+    plt.title(f"Distribution of {target_col} by {by_col} - {test_count} Samples")
+    plt.xlabel(target_col)
     plt.ylabel("Count")
     plt.show()
 
